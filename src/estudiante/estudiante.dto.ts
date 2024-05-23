80a7d4538e8e79cdf2ecc517c07b83d782d09a2a
SebastianCasanova4 <s.casanovao@uniandes.edu.co>
2024-05-23 10:50:52 -0500

    final clase

--- src/estudiante/estudiante.dto.ts
+++ src/estudiante/estudiante.dto.ts
@@ -1,1 +1,8 @@
-export class EstudianteDto {}
+import {IsNotEmpty, IsString, IsUrl} from 'class-validator';
+
+export class EstudianteDto {
+    @IsString()
+    @IsNotEmpty()
+    readonly id: string;
+     //Hay que hacer lo mismo para todos los dto y todas las variables
+}
